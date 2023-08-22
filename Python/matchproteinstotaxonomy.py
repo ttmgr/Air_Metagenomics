029031b81a6dada401bd917c8413c769c493c884
ttmgr <74190928+ttmgr@users.noreply.github.com>
2023-08-22 11:27:30 +0200

    Update matchproteinstotaxonomy.py

--- Python/matchproteinstotaxonomy.py
+++ Python/matchproteinstotaxonomy.py
@@ -1,9 +1,20 @@
+import os
 import pandas as pd
 from Bio import Entrez
 import time
+import socket
+import numpy as np
 
-# Always tell NCBI who you are
-Entrez.email = "your_email@example.com"
+Entrez.email = "your@email"
+
+def is_connected():
+    """Check if there is an internet connection"""
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False
 
 def get_taxonomy(protein_id):
     """Fetch taxonomy for a given protein accession number"""
@@ -11,44 +22,51 @@ def get_taxonomy(protein_id):
     record = Entrez.read(search)
     return record[0]["GBSeq_organism"], record[0]["GBSeq_taxonomy"]
 
-# Load the TSV file
-data = pd.read_csv("diamond_output.tsv", sep='\t', header=None)
+# Define sample name
+sample_name = "diamondoutput_samplename"
 
-# Define the column names
-column_names = ["query_seq_id", "subject_seq_id", "pct_identical_matches", "alignment_length", "num_mismatches", 
+# Load data from the original file
+data = pd.read_csv(f"{sample_name}.tsv", sep='\t', header=None)
+data.columns = ["query_seq_id", "subject_seq_id", "pct_identical_matches", "alignment_length", "num_mismatches", 
                 "num_gap_opens", "start_alignment_query", "end_alignment_query", "start_alignment_subject", 
                 "end_alignment_subject", "e_value", "bit_score"]
+data["organism"] = np.nan
+data["taxonomy"] = np.nan
 
-# Add column names to the dataframe
-data.columns = column_names
+# Load the checkpoint file if it exists
+if os.path.exists(f"{sample_name}_checkpoint.tsv"):
+    checkpoint_data = pd.read_csv(f"{sample_name}_checkpoint.tsv", sep='\t')
+else:
+    checkpoint_data = data.copy()
 
-# Create new columns for organism and taxonomy
-data["organism"] = ""
-data["taxonomy"] = ""
-
-# Get the total number of rows
 total_rows = len(data)
 
-# Go through each row in the dataframe
-for i, row in data.iterrows():
-    # Get the subject sequence ID
+for i, row in checkpoint_data.iterrows():
     subject_seq_id = row["subject_seq_id"]
 
     print(f"Processing row {i+1} of {total_rows}: {subject_seq_id}")
 
-    # Get the taxonomy
+    # Skip rows that have already been processed
+    if pd.notna(row["organism"]) and pd.notna(row["taxonomy"]):
+        continue
+
+    while not is_connected():
+        print("  Waiting for internet connection...")
+        time.sleep(5)
+
     try:
         organism, taxonomy = get_taxonomy(subject_seq_id)
-        data.at[i, "organism"] = organism
-        data.at[i, "taxonomy"] = taxonomy
+        checkpoint_data.at[i, "organism"] = organism
+        checkpoint_data.at[i, "taxonomy"] = taxonomy
         print(f"  Fetched taxonomy for {subject_seq_id}")
+
+        # Save progress after each successful fetch
+        checkpoint_data.to_csv(f"{sample_name}_checkpoint.tsv", sep='\t', index=False)
     except Exception as e:
         print(f"  Couldn't fetch taxonomy for {subject_seq_id}: {e}")
 
-    # To prevent overwhelming the NCBI servers, sleep for a little bit between each request
-    time.sleep(0.2)
-
-# Save the dataframe to a new TSV file
-data.to_csv("diamond_output_with_taxonomy.tsv", sep='\t', index=False)
+# Save the final result
+checkpoint_data.to_csv(f"{sample_name}_with_taxonomy.tsv", sep='\t', index=False)
 
 print("Finished processing all rows.")
+
