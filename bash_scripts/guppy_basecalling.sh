c026c6b2b1798b0640041961cd8b6bb916149e2e
ttmgr <74190928+ttmgr@users.noreply.github.com>
2025-07-22 10:22:26 +0200

    Delete bash_scripts directory

--- bash_scripts/guppy_basecalling.sh
+++
@@ -1,7 +0,0 @@
-guppy_basecaller \
-  --input_path /path/to/fast5_files \
-  --save_path /path/to/output_folder \
-  --recursive \
-  --config dna_r10.4.1_e8.2_400bps_hac.cfg \
-  --cpu_threads_per_caller 4 \
-  --num_callers 4 \
