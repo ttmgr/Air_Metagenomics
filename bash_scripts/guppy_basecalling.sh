d91408046ccdf80eca9949fcd22b2783669e5ed4
ttmgr <74190928+ttmgr@users.noreply.github.com>
2025-07-22 09:05:13 +0200

    Create guppy_basecalling.sh

---
+++ bash_scripts/guppy_basecalling.sh
@@ -0,0 +1,8 @@
+'''bash
+guppy_basecaller \
+  --input_path /path/to/fast5_files \
+  --save_path /path/to/output_folder \
+  --config dna_r10.4.1_e8.2_400bps_hac.cfg \
+  --cpu_threads_per_caller 4 \
+  --num_callers 4 \
+'''
